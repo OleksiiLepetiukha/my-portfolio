161b5eb17e377ee48e092d3b50f16ea0a3e6999d
DESKTOP-IV1O8UM\Oleksii <alekseylepetukha@icloud.com>
2024-03-11 13:46:21 +0000

    added text change

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 // Background transition
 const target = document.querySelector('.target');
+const targetTxt = document.querySelector('.target-text');
 
 target.addEventListener('click', changeTarget);
 
 function changeTarget() {
     target.classList.toggle('js-target');
+    if (target.classList.length > 1) {
+        targetTxt.textContent = 'Click and back outside';
+    } else {
+        targetTxt.textContent = 'Click and look inside';
+    }
 }
